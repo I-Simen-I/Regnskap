5605e58e00a481c035c27629f45f08a67b6670d8
Simen Sohol <simen.so@outlook.com>
2016-10-10 09:02:19 +0200

    RE-4: La til et enkelt sikkerhetslag på REST apiet

---
+++ src/main/java/no/regnskap/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package no.regnskap.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
