2bf543b28ebc6f789cf147be34f5caa26fa96fd0
Simen Søhol <simen.so@outlook.com>
2017-01-26 15:20:53 +0100

    Bruker statisk import av DomainBuilder

--- src/test/java/no/regnskap/domain/UserTest.java
+++ src/test/java/no/regnskap/domain/UserTest.java
@@ -1,8 +1,8 @@
 package no.regnskap.domain;
 
-import no.regnskap.domain.builders.DomainBuilder;
 import org.junit.Test;
 
+import static no.regnskap.domain.builders.DomainBuilder.aUser;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -10,34 +10,34 @@ public class UserTest {
 
     @Test
     public void isEqualWhenUserHaveSameId() throws Exception {
-        User u1 = DomainBuilder.aUser().with(1234L).build();
+        User u1 = aUser().with(1234L).build();
 
-        User u2 = DomainBuilder.aUser().with(1234L).build();
+        User u2 = aUser().with(1234L).build();
 
         assertTrue(u1.equals(u2));
     }
 
     @Test
     public void isNotEqualWhenUserHaveSameId() throws Exception {
-        User u1 = DomainBuilder.aUser().with(1234L).build();
+        User u1 = aUser().with(1234L).build();
 
-        User u2 = DomainBuilder.aUser().with(1233L).build();
+        User u2 = aUser().with(1233L).build();
 
         assertFalse(u1.equals(u2));
     }
 
     @Test
     public void isNotEqualWhenUserIdIsNull() throws Exception {
-        User u1 = DomainBuilder.aUser().with(1234L).build();
+        User u1 = aUser().with(1234L).build();
 
-        User u2 = DomainBuilder.aUser().build();
+        User u2 = aUser().build();
 
         assertFalse(u1.equals(u2));
     }
 
     @Test
     public void isNotEqualWhenUserIsNull() throws Exception {
-        User u1 = DomainBuilder.aUser().with(1234L).build();
+        User u1 = aUser().with(1234L).build();
 
         assertFalse(u1.equals(null));
     }
